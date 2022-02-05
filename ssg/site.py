15eefbe92a02decc34bdf9b49e436fb23e33e5dd
shaileshkr7 <shailesh.aspire@gmail.com>
2022-02-06 04:14:11 +0530

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -3,7 +3,7 @@ class Site:
     def __init__(self,source,dest,parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-        parsers = []
+        self.parsers = []
     def create_dir(self,path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True,exist_ok=True)
@@ -14,12 +14,12 @@ class Site:
                 self.create_dir(path)
             elif path is file:
                 run_parser(path)
-    def load_parser(extension):
+    def load_parser(self,extension):
         for parser in self.parsers:
             if(valid_exttension(extension)):
                 return parser
-    def run_parser(path):
-        parser = load_parser(path.suffix)
+    def run_parser(self,path):
+        parser = self.load_parser(path.suffix)
         if(parser is not None):
             parser.parse(path,source,dest)
         else:
