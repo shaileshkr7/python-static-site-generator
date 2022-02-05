beb9d8f3513b8f41425e9736be6267ea9ca76c37
shaileshkr7 <shailesh.aspire@gmail.com>
2022-02-06 04:27:34 +0530

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -16,15 +16,15 @@ class Parser:
             return file.read()
 
     def write(self,path,dest,content,ext=".html"):
-        full_path=self.dest/path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self,path,source,dest):
         shutil.copy2(path,dest/path.relative_to(source))
 
-    class ResourceParser(Parser):
-        extensions = [".jpg",".png",".gif",".css",".html"]
+class ResourceParser(Parser):
+    extensions = [".jpg",".png",".gif",".css",".html"]
 
     def parse(self,path,source,dest):
         self.copy(path,source,dest)
--- ssg/site.py
+++ ssg/site.py
@@ -12,15 +12,16 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            elif path is file:
-                run_parser(path)
+            elif path is_file():
+                self.run_parser(path)
     def load_parser(self,extension):
         for parser in self.parsers:
-            if(valid_exttension(extension)):
+            if parser.valid_extension(extension):
                 return parser
+
     def run_parser(self,path):
         parser = self.load_parser(path.suffix)
-        if(parser is not None):
+        if parser is not None :
             parser.parse(path,source,dest)
         else:
             print("Not Implemented")
