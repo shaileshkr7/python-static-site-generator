530230ed8deba74add0bf5f70a14476c2b6dad50
shaileshkr7 <shailesh.aspire@gmail.com>
2022-02-06 04:28:42 +0530

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -12,7 +12,7 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            elif path is_file():
+            elif path.is_file():
                 self.run_parser(path)
     def load_parser(self,extension):
         for parser in self.parsers:
